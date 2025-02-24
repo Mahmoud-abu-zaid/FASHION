95550f253cd45a8db79f1b1303de3bb6023a759e
Mahmoud-abu-zaid <v.i.p.omlat@gmail.com>
2025-02-24 20:18:59 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Footer from "./components/layouts/Footer";
+import Header from "./components/layouts/Header";
+
+function App() {
+  return <>
+  <Header/>
+  <Footer/>
+  </>;
+}
+
+export default App;
---
+++ src/components/layouts/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router";
+
+export default function Header() {
+  return <>
+  <div>
+    <div>
+      <Link to="/">FASHION</Link>
+    </div>
+  </div>
+  </>;
+}
