8037b526254dcd7191de5f41adec80369bba1a48
Mahmoud-abu-zaid <v.i.p.omlat@gmail.com>
2025-02-24 20:39:07 +0200

    Add:Header

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,11 +1,35 @@
 import { Link } from "react-router";
 
 export default function Header() {
-  return <>
-  <div>
-    <div>
-      <Link to="/">FASHION</Link>
-    </div>
-  </div>
-  </>;
+  return (
+    <>
+      <div>
+        <nav className="flex justify-between p-5 fixed right-0 left-0">
+          <div className="">
+            <Link to="/">
+              <img className="inline-block p-2 pt-0" src="/img/Vector (1).png" alt="" />
+              <b className=" text-[25px]">FASHION</b>
+            </Link>
+          </div>
+          <ul className="md:flex ">
+            <li>
+              <Link className="p-3">CATALOGUE</Link>
+            </li>
+            <li>
+              <Link className="p-3">FASHION</Link>
+            </li>
+            <li>
+              <Link className="p-3">FAVOURITE</Link>
+            </li>
+            <li>
+              <Link className="p-3">LIFESTYLE</Link>
+            </li>
+            <li>
+              <Link className="p-3">SIGN UP</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
 }
